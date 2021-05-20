9d3b11651c64acbc329b5120d3a91b0cd6994c41
dekapaan <terblancheronald@gmail.com>
2021-05-20 13:37:51 +0200

    changed font

--- main.py
+++ main.py
@@ -11,40 +11,43 @@ class TicketSales:
         self.master.config(bg='#191923')
 
         # Cell number label and entry
-        self.cell_entry_lable = Label(self.master, text='Enter CellNumber:', bg='#191923', fg='#BDBF09')
-        self.cell_entry = Entry(self.master, bg='#BDBF09', fg='#191923')
+        self.cell_entry_lable = Label(self.master, text='Enter CellNumber:', bg='#191923', fg='#BDBF09', font="monospace 10")
+        self.cell_entry = Entry(self.master, bg='#BDBF09', fg='#191923', font="monospace 10")
         self.cell_entry_lable.place(x=10, y=10)
-        self.cell_entry.place(x=200, y=10)
+        self.cell_entry.place(x=220, y=10)
 
         # Ticket type label and option box for ticket type
-        self.ticket_label = Label(self.master, text='Select Ticket Category:', bg='#191923', fg='#BDBF09')
+        self.ticket_label = Label(self.master, text='Select Ticket Category:', bg='#191923', fg='#BDBF09', font="monospace 10")
         self.options = ['Soccer', 'Movie', 'Theater']
         self.variable = StringVar()
         self.variable.set('Select Ticket')
         self.ticket_op = OptionMenu(master, self.variable, *self.options)
+        self.ticket_op.config(font="monospace 10")
+        menu = self.master.nametowidget(self.ticket_op.menuname)
+        menu.config(font='monospace 10')
         self.ticket_label.place(x=10, y=50)
-        self.ticket_op.place(x=200, y=45)
+        self.ticket_op.place(x=220, y=45)
 
         # Ticket number label and ticket number spinbox
-        self.ticket_no_label = Label(self.master, text='Number of Tickets Bought:', bg='#191923', fg='#BDBF09')
-        self.ticket_spinbox = Spinbox(self.master, width=10, from_=0, to=100, bg='#BDBF09', fg='#191923')
+        self.ticket_no_label = Label(self.master, text='Number of Tickets Bought:', bg='#191923', fg='#BDBF09', font="monospace 10")
+        self.ticket_spinbox = Spinbox(self.master, width=10, from_=0, to=100, bg='#BDBF09', fg='#191923', font="monospace 10")
         self.ticket_no_label.place(x=10, y=90)
-        self.ticket_spinbox.place(x=200, y=90)
+        self.ticket_spinbox.place(x=220, y=90)
 
         # Calculate button
-        self.calc_button = Button(self.master, text='Calculate Ticket', command=self.calc_prepayment)
-        self.clear_button = Button(self.master, text='Clear Entries', command=self.clear)
-        self.calc_button.place(x=70, y=180)
-        self.clear_button.place(x=230, y=180)
+        self.calc_button = Button(self.master, text='Calculate Ticket', command=self.calc_prepayment, font="monospace 10")
+        self.clear_button = Button(self.master, text='Clear Entries', command=self.clear, font="monospace 10")
+        self.calc_button.place(x=40, y=180)
+        self.clear_button.place(x=225, y=180)
 
         # Payment info frame
         self.frame = Frame(self.master, width=318, height=180, bg='#BDBF09')
         self.frame.place(x=40, y=230)
 
         # Payment info
-        self.amount_pay = Label(self.frame, text='', fg='#191923', bg='#BDBF09')
-        self.reserve = Label(self.frame, text='', fg='#191923', bg='#BDBF09')
-        self.cell_label = Label(self.frame, text='', fg='#191923', bg='#BDBF09')
+        self.amount_pay = Label(self.frame, text='', fg='#191923', bg='#BDBF09', font="monospace 10")
+        self.reserve = Label(self.frame, text='', fg='#191923', bg='#BDBF09', font="monospace 10")
+        self.cell_label = Label(self.frame, text='', fg='#191923', bg='#BDBF09', font="monospace 10")
         self.amount_pay.place(x=10, y=30)
         self.reserve.place(x=10, y=70)
         self.cell_label.place(x=10, y=110)
