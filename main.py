aa243aa901fd436e1224a58be3c4d8daeaae9543
dekapaan <terblancheronald@gmail.com>
2021-05-20 11:29:33 +0200

    Improving visuals

--- main.py
+++ main.py
@@ -2,81 +2,92 @@ from tkinter import *
 from tkinter import messagebox
 
 
-class TicketSales(object):
+class TicketSales:
     def __init__(self, master):
         # Initialise window
         self.master = master
         self.master.title('TicketSales')
         self.master.geometry('400x450')
+        self.master.config(bg='#191923')
 
         # Cell number label and entry
-        self.cell_entry_lable = Label(self.master, text='Enter CellNumber:')
-        self.cell_entry = Entry(self.master)
+        self.cell_entry_lable = Label(self.master, text='Enter CellNumber:', bg='#191923', fg='#BDBF09')
+        self.cell_entry = Entry(self.master, bg='#BDBF09', fg='#191923')
         self.cell_entry_lable.place(x=10, y=10)
         self.cell_entry.place(x=200, y=10)
 
-        self.ticket_label = Label(self.master, text='Select Ticket Category:')
+        # Ticket type label and option box for ticket type
+        self.ticket_label = Label(self.master, text='Select Ticket Category:', bg='#191923', fg='#BDBF09')
         self.options = ['Soccer', 'Movie', 'Theater']
-        self.variable = StringVar(self.master)
+        self.variable = StringVar()
         self.variable.set('Select Ticket')
         self.ticket_op = OptionMenu(master, self.variable, *self.options)
         self.ticket_label.place(x=10, y=50)
         self.ticket_op.place(x=200, y=45)
 
-        self.ticket_no_label = Label(self.master, text='Number of Tickets Bought:')
-        self.ticket_spinbox = Spinbox(self.master, width=10, from_=0, to=100)
+        # Ticket number label and ticket number spinbox
+        self.ticket_no_label = Label(self.master, text='Number of Tickets Bought:', bg='#191923', fg='#BDBF09')
+        self.ticket_spinbox = Spinbox(self.master, width=10, from_=0, to=100, bg='#BDBF09', fg='#191923')
         self.ticket_no_label.place(x=10, y=90)
         self.ticket_spinbox.place(x=200, y=90)
 
+        # Calculate button
         self.calc_button = Button(self.master, text='Calculate Ticket', command=self.calc_prepayment)
         self.clear_button = Button(self.master, text='Clear Entries', command=self.clear)
         self.calc_button.place(x=70, y=180)
         self.clear_button.place(x=230, y=180)
 
-        self.border1 = Label(self.master, text='---------------------------------------------------------------')
-        self.border2 = Label(self.master, text='---------------------------------------------------------------')
-        self.border1.place(x=40, y=220)
-        self.border2.place(x=40, y=380)
+        # Payment info frame
+        self.frame = Frame(self.master, width=318, height=180, bg='#BDBF09')
+        self.frame.place(x=40, y=230)
 
-        self.amount_pay = Label(self.master, text='')
-        self.reserve = Label(self.master, text='')
-        self.cell_label = Label(self.master, text='')
-        self.amount_pay.place(x=50, y=260)
-        self.reserve.place(x=50, y=300)
-        self.cell_label.place(x=50, y=340)
+        # Payment info
+        self.amount_pay = Label(self.frame, text='', fg='#191923', bg='#BDBF09')
+        self.reserve = Label(self.frame, text='', fg='#191923', bg='#BDBF09')
+        self.cell_label = Label(self.frame, text='', fg='#191923', bg='#BDBF09')
+        self.amount_pay.place(x=10, y=30)
+        self.reserve.place(x=10, y=70)
+        self.cell_label.place(x=10, y=110)
 
     def calc_prepayment(self):
+        # Initialise variables and constants
         ticket_no = int(self.ticket_spinbox.get())
         vat = 0.14
+
+        # If try fails, traps error and shows ValueError messagebox
         try:
-            int(self.cell_entry.get())
+            int(self.cell_entry.get())  # To check if cell no is not float or string
             if len(self.cell_entry.get()) < 10 or len(self.cell_entry.get()) > 10:
-                raise ValueError
+                raise ValueError  # raise ValueError if cell number is </> than 10 digits
 
             elif self.variable.get() == 'Select Ticket':
-                raise ValueError
+                raise ValueError  # If ticket type not selected, ValueError raised
 
             elif int(self.ticket_spinbox.get()) == 0:
-                raise ValueError
+                raise ValueError  # If spinbox value == 0, raise ValueError
 
+            # Soccer type calculation total price calculation
             elif self.variable.get() == 'Soccer':
                 price = 40  # in Rands
                 price_pay = (price * ticket_no) + (price * ticket_no * vat)
                 text = ('Amount Payable: R{}'.format(price_pay))
                 self.amount_pay.config(text=text)
 
+            # Movie type total price calculation
             elif self.variable.get() == 'Movie':
                 price = 75  # in Rands
                 price_pay = (price * ticket_no) + (price * ticket_no * vat)
                 text = ('Amount Payable: R{}'.format(price_pay))
                 self.amount_pay.config(text=text)
 
+            # Theater type total price calculation
             elif self.variable.get() == 'Theater':
                 price = 100  # in Rands
                 price_pay = (price * ticket_no) + (price * ticket_no * vat)
                 text = ('Amount Payable: R{}'.format(price_pay))
                 self.amount_pay.config(text=text)
 
+            # Displaying amount payable, reservation and cell number
             reserve_text = 'Reservation for {} for {}'.format(self.variable.get(), ticket_no)
             cell_text = 'was done by {}'.format(self.cell_entry.get())
             self.reserve.config(text=reserve_text)
@@ -85,6 +96,7 @@ class TicketSales(object):
         except ValueError:
             messagebox.showerror(message='Invalid combination')
 
+    # Clear all entry fields
     def clear(self):
         self.cell_entry.delete(0, END)
         self.cell_entry.focus()
@@ -97,5 +109,6 @@ class TicketSales(object):
 
 
 root = Tk()
-TicketSales(root)
+TicketSales(root)   # Instance of class
 root.mainloop()
+
